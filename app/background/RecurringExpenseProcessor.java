f29560106a8207e083a147dd99c05ca9a79e9947
Paul Fauchon <p.fauchon@gmail.com>
2015-07-05 18:22:55 +0800

    Send notification on recurring expense

--- app/background/RecurringExpenseProcessor.java
+++ app/background/RecurringExpenseProcessor.java
@@ -1,5 +1,6 @@
 package background;
 
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -7,6 +8,7 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import notifications.Notifications;
 import models.Expense;
 import models.RecurringExpense;
 import play.Logger;
@@ -25,6 +27,7 @@ public class RecurringExpenseProcessor implements Runnable {
 	private ExecutorService exec = Executors.newSingleThreadExecutor();
 	private boolean refresh = true;
 	private SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+	DecimalFormat df2 = new DecimalFormat("#,###,###,##0.00");
 
 	@Inject
 	public RecurringExpenseProcessor(ApplicationLifecycle lifeCycle) {
@@ -63,6 +66,8 @@ public class RecurringExpenseProcessor implements Runnable {
 	@Override
 	public void run() {
 		while (refresh) {
+			StringBuilder str = new StringBuilder();
+			boolean sendNotification = false;
 			try {
 				List<RecurringExpense> recurringExpenses = getExpenses();
 				while (recurringExpenses.size() > 0) {
@@ -83,6 +88,27 @@ public class RecurringExpenseProcessor implements Runnable {
 
 							recurring.save();
 							Logger.info("Expense added, next occurence:[{}]", recurring.getNextOccurrence());
+
+							String type = "";
+							switch (recurring.getType()) {
+							case RecurringExpense.TYPE_DAILY:
+								type = "daily";
+								break;
+							case RecurringExpense.TYPE_WEEKLY:
+								type = "weekly";
+								break;
+							case RecurringExpense.TYPE_MONTHLY:
+								type = "monthly";
+								break;
+							case RecurringExpense.TYPE_YEARLY:
+								type = "yearly";
+								break;
+
+							}
+
+							str.append(df2.format(recurring.getAmount())).append("paid as ").append(type).append("  expense, next occurrence: ").append(recurring.getNextOccurrence());
+							str.append("\n");
+							sendNotification = true;
 						} catch (Exception e) {
 							Logger.info("Error processing the occurrences", e);
 						}
@@ -90,6 +116,9 @@ public class RecurringExpenseProcessor implements Runnable {
 
 					recurringExpenses = getExpenses();
 				}
+
+				if (sendNotification)
+					Notifications.send("Recurring expense summary", str.toString());
 
 				try {
 					Thread.sleep(10000 * 3600);
