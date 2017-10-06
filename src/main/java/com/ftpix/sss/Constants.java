b5a3a629ff20792cc2540feed91f9e34d7e4ffb6
Paul Fauchon <p.fauchon@gmail.com>
2017-10-07 07:53:12 +0800

    allow to set up application with -D parameters move web source to /src/main/web to remove the web soruce and node modules from the final jar add maven build steps to run npm command while building

--- src/main/java/com/ftpix/sss/Constants.java
+++ src/main/java/com/ftpix/sss/Constants.java
@@ -1,5 +1,6 @@
 package com.ftpix.sss;
 
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class Constants {
@@ -8,11 +9,45 @@ public class Constants {
     public final static int HTTP_PORT;
     public static final String JSON = "application/json";
 
-    static {
-        ResourceBundle rs = ResourceBundle.getBundle("config");
 
-        DB_PATH = rs.getString("db.url");
-        SALT = rs.getString("salt");
-        HTTP_PORT = Integer.parseInt(rs.getString("http.port"));
+    public static final String CFG_DB_URL = "db.url";
+
+    public static final String CFG_SALT = "salt";
+
+    public static final String CFG_PORT = "http.port";
+
+    static {
+
+        String dbUrl, salt;
+        int httpPort;
+
+        Optional<String> dbPathSysProp = Optional.ofNullable(System.getProperty(CFG_DB_URL));
+        Optional<String> saltSysProp = Optional.ofNullable(System.getProperty(CFG_SALT));
+        Optional<Integer> portSysProp = Optional.ofNullable(System.getProperty(CFG_PORT)).map(Integer::valueOf);
+
+
+        if (dbPathSysProp.isPresent() && saltSysProp.isPresent() && portSysProp.isPresent()) {
+            dbUrl = dbPathSysProp.get();
+            salt = saltSysProp.get();
+            httpPort = portSysProp.get();
+
+        } else {
+
+            ResourceBundle rs = ResourceBundle.getBundle("config");
+
+            dbUrl = rs.getString(CFG_DB_URL);
+            salt = rs.getString(CFG_SALT);
+            httpPort = Integer.parseInt(rs.getString(CFG_PORT));
+        }
+
+        DB_PATH = dbUrl;
+        SALT = salt;
+        HTTP_PORT = httpPort;
+
+
+        System.out.println("SpendSpentSpent config ====================");
+        System.out.println("DB URL = " + DB_PATH);
+        System.out.println("HTTP PORT = " + HTTP_PORT);
+        System.out.println("SALT = " + SALT);
     }
 }
