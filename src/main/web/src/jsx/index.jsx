1abce06c117a05f657192479df8b0a70676db817
Paul Fauchon <p.fauchon@gmail.com>
2020-10-12 12:44:38 +0800

    fix PWA

--- src/main/web/src/jsx/index.jsx
+++ src/main/web/src/jsx/index.jsx
@@ -19,15 +19,6 @@ String.prototype.format = function () {
 };
 
 
-// CODELAB: Register service worker.
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('../service-worker.js')
-            .then((reg) => {
-                console.log('Service worker registered.', reg);
-            });
-    });
-}
 
 render((
     <BrowserRouter>
