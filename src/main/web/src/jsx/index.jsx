037552941b9ded856c201c517838ab11f3bc3cd4
Paul Fauchon <p.fauchon@gmail.com>
2020-10-12 12:20:35 +0800

    add basic PWA support

--- src/main/web/src/jsx/index.jsx
+++ src/main/web/src/jsx/index.jsx
@@ -19,6 +19,16 @@ String.prototype.format = function () {
 };
 
 
+// CODELAB: Register service worker.
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('../service-worker.js')
+            .then((reg) => {
+                console.log('Service worker registered.', reg);
+            });
+    });
+}
+
 render((
     <BrowserRouter>
         <div>
