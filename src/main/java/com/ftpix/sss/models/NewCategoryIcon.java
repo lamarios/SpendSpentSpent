6d8a724d97d4846901064322050c92dbae0e7948
Paul Fauchon <p.fauchon@gmail.com>
2020-12-05 11:12:43 +0800

    add brands icons

--- src/main/java/com/ftpix/sss/models/NewCategoryIcon.java
+++ src/main/java/com/ftpix/sss/models/NewCategoryIcon.java
@@ -8,7 +8,11 @@ public enum NewCategoryIcon {
 
     food(Constants.SHOPPING, new String[]{"food", "eat", "restaurant"}),
     gas(Constants.TRANSPORTS, new String[]{"petrol", "gas", "diesel"}),
-    ;
+    amazon(Constants.BRANDS, new String[]{"amazon", "prime", "aws", "amazon web services", "streaming"}),
+    apple(Constants.BRANDS, new String[]{"apple", "icloud", "iphone", "ipad", "mac", "ipod", "streaming", "apple music"}),
+    netflix(Constants.BRANDS, new String[]{"netflix", "streaming"}),
+    google(Constants.BRANDS, new String[]{"google", "suite", "youtube", "gmail"}),
+    youtube(Constants.BRANDS, new String[]{"youtube", "streaming"});
 
     private final String category;
     private final String[] searchTerms;
@@ -29,5 +33,6 @@ public enum NewCategoryIcon {
     private static class Constants {
         public static final String SHOPPING = "shopping";
         public static final String TRANSPORTS = "transports";
+        public static final String BRANDS = "brands";
     }
 }
