2c35a0c08e4b84a7666eba639efdddbe9cbe1ce1
Paul Fauchon <paul@fauchon.dev>
2021-12-27 21:01:53 +0800

    don't release ios automatically

--- src/main/app/build-ios.sh
+++ src/main/app/build-ios.sh
@@ -1,4 +1,12 @@
+OPTS=""
+if [ -z ${DRONE_TAG+x} ]; then
+  echo "Build number: ${DRONE_TAG}"
+  OPTS="--build-number ${DRONE_TAG}"
+else
+  echo "No build number, using default"
+fi
+
 flutter clean \
         && rm ios/Podfile.lock pubspec.lock || true\
         && rm -rf ios/Pods ios/Runner.xcworkspace || true \
-        && flutter build ios --release --build-number ${DRONE_TAG}
+        && flutter build ios --release ${OPTS}
