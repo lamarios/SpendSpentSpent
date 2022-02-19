47892ec5a21a028366c5757617ef2604db261b89
Paul Fauchon <paul@fauchon.dev>
2022-02-19 16:31:12 +0800

    change animation for categories

--- src/main/app/lib/components/categories/categoryGridItem.dart
+++ src/main/app/lib/components/categories/categoryGridItem.dart
@@ -11,6 +11,7 @@ import 'package:spend_spent_spent/utils/dialogs.dart';
 import 'package:spend_spent_spent/views/addExpense.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:tinycolor2/tinycolor2.dart';
 
 class CategoryGridItem extends StatefulWidget {
   Category category;
@@ -24,6 +25,8 @@ class CategoryGridItem extends StatefulWidget {
 class CategoryGridItemState extends State<CategoryGridItem> with AfterLayoutMixin {
   double scale = 0.5;
   double opacity = 0;
+  Offset offset = Offset(0, 1);
+  BoxDecoration container = BoxDecoration(borderRadius: defaultBorder, color: Colors.blue.withOpacity(0));
 
   showDialog(BuildContext context) {
     showModal(context: context, builder: (context) => Card(margin: getInsetsForMaxSize(MediaQuery.of(context), maxWidth: 350, maxHeight: 650), child: AddExpense(category: widget.category)));
@@ -34,32 +37,38 @@ class CategoryGridItemState extends State<CategoryGridItem> with AfterLayoutMixi
     AppColors colors = get(context);
     return GestureDetector(
       onTap: () => showDialog(context),
-      child: AnimatedOpacity(
+      child: AnimatedContainer(
+        alignment: Alignment.center,
+        decoration: container,
         duration: panelTransition,
-        opacity: opacity,
-        curve: Curves.easeInOutQuart,
-        child: AnimatedScale(
-          duration: panelTransition,
-          scale: scale,
-          curve: Curves.easeInOut,
-          alignment: Alignment.center,
-          child: Container(
-            alignment: Alignment.center,
-            decoration: BoxDecoration(borderRadius: defaultBorder, gradient: defaultGradient(context)),
-            child: Hero(tag: widget.category.icon!, child: getIcon(widget.category.icon!, size: 40, color: colors.iconOnMain)),
-          ),
-        ),
+        child: AnimatedOpacity(
+            duration: panelTransition,
+            opacity: opacity,
+            curve: Curves.easeInOutQuart,
+            child: AnimatedSlide(
+                duration: panelTransition,
+                // scale: scale,
+                curve: Curves.easeInOut,
+                offset: offset,
+                // alignment: Alignment.center,
+                child: Hero(tag: widget.category.icon!, child: getIcon(widget.category.icon!, size: 40, color: colors.iconOnMain)))),
       ),
     );
   }
 
   @override
   void afterFirstLayout(BuildContext context) {
+    AppColors colors = get(context);
+    setState(() {
+      container = BoxDecoration(borderRadius: defaultBorder, color: colors.main);
+    });
     Random rand = Random();
     Future.delayed(Duration(milliseconds: rand.nextInt(150)), () {
       setState(() {
         scale = 1.0;
         opacity = 1.0;
+        offset += const Offset(0, -1);
+        container = BoxDecoration(borderRadius: defaultBorder, color: TinyColor(colors.main).darken(10));
       });
     });
   }
