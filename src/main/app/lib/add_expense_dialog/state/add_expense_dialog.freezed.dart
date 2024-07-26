903a99cee3cfc351ce3e7f9c3c67beb905de97b5
nixos script <paul@fauchon.dev>
2024-07-26 19:27:33 +0800

    add freezed

---
+++ src/main/app/lib/add_expense_dialog/state/add_expense_dialog.freezed.dart
@@ -0,0 +1,443 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'add_expense_dialog.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$AddExpenseDialogState {
+  String get value => throw _privateConstructorUsedError;
+  String get valueFrom => throw _privateConstructorUsedError;
+  DateTime get expenseDate => throw _privateConstructorUsedError;
+  String get expenseNote => throw _privateConstructorUsedError;
+  bool get gettingLocation => throw _privateConstructorUsedError;
+  bool get useLocation => throw _privateConstructorUsedError;
+  CurrencyConversion? get currencyConversion =>
+      throw _privateConstructorUsedError;
+  bool get showCurrencyConversion => throw _privateConstructorUsedError;
+  bool get saving => throw _privateConstructorUsedError;
+  Widget? get savingIcon => throw _privateConstructorUsedError;
+  List<String> get noteSuggestions => throw _privateConstructorUsedError;
+  LocationData? get location => throw _privateConstructorUsedError;
+  dynamic get error => throw _privateConstructorUsedError;
+  StackTrace? get stackTrace => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $AddExpenseDialogStateCopyWith<AddExpenseDialogState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $AddExpenseDialogStateCopyWith<$Res> {
+  factory $AddExpenseDialogStateCopyWith(AddExpenseDialogState value,
+          $Res Function(AddExpenseDialogState) then) =
+      _$AddExpenseDialogStateCopyWithImpl<$Res, AddExpenseDialogState>;
+  @useResult
+  $Res call(
+      {String value,
+      String valueFrom,
+      DateTime expenseDate,
+      String expenseNote,
+      bool gettingLocation,
+      bool useLocation,
+      CurrencyConversion? currencyConversion,
+      bool showCurrencyConversion,
+      bool saving,
+      Widget? savingIcon,
+      List<String> noteSuggestions,
+      LocationData? location,
+      dynamic error,
+      StackTrace? stackTrace});
+}
+
+/// @nodoc
+class _$AddExpenseDialogStateCopyWithImpl<$Res,
+        $Val extends AddExpenseDialogState>
+    implements $AddExpenseDialogStateCopyWith<$Res> {
+  _$AddExpenseDialogStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? value = null,
+    Object? valueFrom = null,
+    Object? expenseDate = null,
+    Object? expenseNote = null,
+    Object? gettingLocation = null,
+    Object? useLocation = null,
+    Object? currencyConversion = freezed,
+    Object? showCurrencyConversion = null,
+    Object? saving = null,
+    Object? savingIcon = freezed,
+    Object? noteSuggestions = null,
+    Object? location = freezed,
+    Object? error = freezed,
+    Object? stackTrace = freezed,
+  }) {
+    return _then(_value.copyWith(
+      value: null == value
+          ? _value.value
+          : value // ignore: cast_nullable_to_non_nullable
+              as String,
+      valueFrom: null == valueFrom
+          ? _value.valueFrom
+          : valueFrom // ignore: cast_nullable_to_non_nullable
+              as String,
+      expenseDate: null == expenseDate
+          ? _value.expenseDate
+          : expenseDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      expenseNote: null == expenseNote
+          ? _value.expenseNote
+          : expenseNote // ignore: cast_nullable_to_non_nullable
+              as String,
+      gettingLocation: null == gettingLocation
+          ? _value.gettingLocation
+          : gettingLocation // ignore: cast_nullable_to_non_nullable
+              as bool,
+      useLocation: null == useLocation
+          ? _value.useLocation
+          : useLocation // ignore: cast_nullable_to_non_nullable
+              as bool,
+      currencyConversion: freezed == currencyConversion
+          ? _value.currencyConversion
+          : currencyConversion // ignore: cast_nullable_to_non_nullable
+              as CurrencyConversion?,
+      showCurrencyConversion: null == showCurrencyConversion
+          ? _value.showCurrencyConversion
+          : showCurrencyConversion // ignore: cast_nullable_to_non_nullable
+              as bool,
+      saving: null == saving
+          ? _value.saving
+          : saving // ignore: cast_nullable_to_non_nullable
+              as bool,
+      savingIcon: freezed == savingIcon
+          ? _value.savingIcon
+          : savingIcon // ignore: cast_nullable_to_non_nullable
+              as Widget?,
+      noteSuggestions: null == noteSuggestions
+          ? _value.noteSuggestions
+          : noteSuggestions // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      location: freezed == location
+          ? _value.location
+          : location // ignore: cast_nullable_to_non_nullable
+              as LocationData?,
+      error: freezed == error
+          ? _value.error
+          : error // ignore: cast_nullable_to_non_nullable
+              as dynamic,
+      stackTrace: freezed == stackTrace
+          ? _value.stackTrace
+          : stackTrace // ignore: cast_nullable_to_non_nullable
+              as StackTrace?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$AddExpenseDialogStateImplCopyWith<$Res>
+    implements $AddExpenseDialogStateCopyWith<$Res> {
+  factory _$$AddExpenseDialogStateImplCopyWith(
+          _$AddExpenseDialogStateImpl value,
+          $Res Function(_$AddExpenseDialogStateImpl) then) =
+      __$$AddExpenseDialogStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String value,
+      String valueFrom,
+      DateTime expenseDate,
+      String expenseNote,
+      bool gettingLocation,
+      bool useLocation,
+      CurrencyConversion? currencyConversion,
+      bool showCurrencyConversion,
+      bool saving,
+      Widget? savingIcon,
+      List<String> noteSuggestions,
+      LocationData? location,
+      dynamic error,
+      StackTrace? stackTrace});
+}
+
+/// @nodoc
+class __$$AddExpenseDialogStateImplCopyWithImpl<$Res>
+    extends _$AddExpenseDialogStateCopyWithImpl<$Res,
+        _$AddExpenseDialogStateImpl>
+    implements _$$AddExpenseDialogStateImplCopyWith<$Res> {
+  __$$AddExpenseDialogStateImplCopyWithImpl(_$AddExpenseDialogStateImpl _value,
+      $Res Function(_$AddExpenseDialogStateImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? value = null,
+    Object? valueFrom = null,
+    Object? expenseDate = null,
+    Object? expenseNote = null,
+    Object? gettingLocation = null,
+    Object? useLocation = null,
+    Object? currencyConversion = freezed,
+    Object? showCurrencyConversion = null,
+    Object? saving = null,
+    Object? savingIcon = freezed,
+    Object? noteSuggestions = null,
+    Object? location = freezed,
+    Object? error = freezed,
+    Object? stackTrace = freezed,
+  }) {
+    return _then(_$AddExpenseDialogStateImpl(
+      value: null == value
+          ? _value.value
+          : value // ignore: cast_nullable_to_non_nullable
+              as String,
+      valueFrom: null == valueFrom
+          ? _value.valueFrom
+          : valueFrom // ignore: cast_nullable_to_non_nullable
+              as String,
+      expenseDate: null == expenseDate
+          ? _value.expenseDate
+          : expenseDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      expenseNote: null == expenseNote
+          ? _value.expenseNote
+          : expenseNote // ignore: cast_nullable_to_non_nullable
+              as String,
+      gettingLocation: null == gettingLocation
+          ? _value.gettingLocation
+          : gettingLocation // ignore: cast_nullable_to_non_nullable
+              as bool,
+      useLocation: null == useLocation
+          ? _value.useLocation
+          : useLocation // ignore: cast_nullable_to_non_nullable
+              as bool,
+      currencyConversion: freezed == currencyConversion
+          ? _value.currencyConversion
+          : currencyConversion // ignore: cast_nullable_to_non_nullable
+              as CurrencyConversion?,
+      showCurrencyConversion: null == showCurrencyConversion
+          ? _value.showCurrencyConversion
+          : showCurrencyConversion // ignore: cast_nullable_to_non_nullable
+              as bool,
+      saving: null == saving
+          ? _value.saving
+          : saving // ignore: cast_nullable_to_non_nullable
+              as bool,
+      savingIcon: freezed == savingIcon
+          ? _value.savingIcon
+          : savingIcon // ignore: cast_nullable_to_non_nullable
+              as Widget?,
+      noteSuggestions: null == noteSuggestions
+          ? _value._noteSuggestions
+          : noteSuggestions // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      location: freezed == location
+          ? _value.location
+          : location // ignore: cast_nullable_to_non_nullable
+              as LocationData?,
+      error: freezed == error
+          ? _value.error
+          : error // ignore: cast_nullable_to_non_nullable
+              as dynamic,
+      stackTrace: freezed == stackTrace
+          ? _value.stackTrace
+          : stackTrace // ignore: cast_nullable_to_non_nullable
+              as StackTrace?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$AddExpenseDialogStateImpl implements _AddExpenseDialogState {
+  const _$AddExpenseDialogStateImpl(
+      {this.value = "",
+      this.valueFrom = "",
+      required this.expenseDate,
+      this.expenseNote = '',
+      this.gettingLocation = false,
+      this.useLocation = false,
+      this.currencyConversion,
+      this.showCurrencyConversion = false,
+      this.saving = false,
+      this.savingIcon,
+      final List<String> noteSuggestions = const [],
+      this.location,
+      this.error,
+      this.stackTrace})
+      : _noteSuggestions = noteSuggestions;
+
+  @override
+  @JsonKey()
+  final String value;
+  @override
+  @JsonKey()
+  final String valueFrom;
+  @override
+  final DateTime expenseDate;
+  @override
+  @JsonKey()
+  final String expenseNote;
+  @override
+  @JsonKey()
+  final bool gettingLocation;
+  @override
+  @JsonKey()
+  final bool useLocation;
+  @override
+  final CurrencyConversion? currencyConversion;
+  @override
+  @JsonKey()
+  final bool showCurrencyConversion;
+  @override
+  @JsonKey()
+  final bool saving;
+  @override
+  final Widget? savingIcon;
+  final List<String> _noteSuggestions;
+  @override
+  @JsonKey()
+  List<String> get noteSuggestions {
+    if (_noteSuggestions is EqualUnmodifiableListView) return _noteSuggestions;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_noteSuggestions);
+  }
+
+  @override
+  final LocationData? location;
+  @override
+  final dynamic error;
+  @override
+  final StackTrace? stackTrace;
+
+  @override
+  String toString() {
+    return 'AddExpenseDialogState(value: $value, valueFrom: $valueFrom, expenseDate: $expenseDate, expenseNote: $expenseNote, gettingLocation: $gettingLocation, useLocation: $useLocation, currencyConversion: $currencyConversion, showCurrencyConversion: $showCurrencyConversion, saving: $saving, savingIcon: $savingIcon, noteSuggestions: $noteSuggestions, location: $location, error: $error, stackTrace: $stackTrace)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$AddExpenseDialogStateImpl &&
+            (identical(other.value, value) || other.value == value) &&
+            (identical(other.valueFrom, valueFrom) ||
+                other.valueFrom == valueFrom) &&
+            (identical(other.expenseDate, expenseDate) ||
+                other.expenseDate == expenseDate) &&
+            (identical(other.expenseNote, expenseNote) ||
+                other.expenseNote == expenseNote) &&
+            (identical(other.gettingLocation, gettingLocation) ||
+                other.gettingLocation == gettingLocation) &&
+            (identical(other.useLocation, useLocation) ||
+                other.useLocation == useLocation) &&
+            (identical(other.currencyConversion, currencyConversion) ||
+                other.currencyConversion == currencyConversion) &&
+            (identical(other.showCurrencyConversion, showCurrencyConversion) ||
+                other.showCurrencyConversion == showCurrencyConversion) &&
+            (identical(other.saving, saving) || other.saving == saving) &&
+            (identical(other.savingIcon, savingIcon) ||
+                other.savingIcon == savingIcon) &&
+            const DeepCollectionEquality()
+                .equals(other._noteSuggestions, _noteSuggestions) &&
+            (identical(other.location, location) ||
+                other.location == location) &&
+            const DeepCollectionEquality().equals(other.error, error) &&
+            (identical(other.stackTrace, stackTrace) ||
+                other.stackTrace == stackTrace));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      value,
+      valueFrom,
+      expenseDate,
+      expenseNote,
+      gettingLocation,
+      useLocation,
+      currencyConversion,
+      showCurrencyConversion,
+      saving,
+      savingIcon,
+      const DeepCollectionEquality().hash(_noteSuggestions),
+      location,
+      const DeepCollectionEquality().hash(error),
+      stackTrace);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$AddExpenseDialogStateImplCopyWith<_$AddExpenseDialogStateImpl>
+      get copyWith => __$$AddExpenseDialogStateImplCopyWithImpl<
+          _$AddExpenseDialogStateImpl>(this, _$identity);
+}
+
+abstract class _AddExpenseDialogState
+    implements AddExpenseDialogState, WithError {
+  const factory _AddExpenseDialogState(
+      {final String value,
+      final String valueFrom,
+      required final DateTime expenseDate,
+      final String expenseNote,
+      final bool gettingLocation,
+      final bool useLocation,
+      final CurrencyConversion? currencyConversion,
+      final bool showCurrencyConversion,
+      final bool saving,
+      final Widget? savingIcon,
+      final List<String> noteSuggestions,
+      final LocationData? location,
+      final dynamic error,
+      final StackTrace? stackTrace}) = _$AddExpenseDialogStateImpl;
+
+  @override
+  String get value;
+  @override
+  String get valueFrom;
+  @override
+  DateTime get expenseDate;
+  @override
+  String get expenseNote;
+  @override
+  bool get gettingLocation;
+  @override
+  bool get useLocation;
+  @override
+  CurrencyConversion? get currencyConversion;
+  @override
+  bool get showCurrencyConversion;
+  @override
+  bool get saving;
+  @override
+  Widget? get savingIcon;
+  @override
+  List<String> get noteSuggestions;
+  @override
+  LocationData? get location;
+  @override
+  dynamic get error;
+  @override
+  StackTrace? get stackTrace;
+  @override
+  @JsonKey(ignore: true)
+  _$$AddExpenseDialogStateImplCopyWith<_$AddExpenseDialogStateImpl>
+      get copyWith => throw _privateConstructorUsedError;
+}
