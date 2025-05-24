a3b5f47c9a554ce12b14f6038b1f95a4679700f6
office-pc nix root <paul@fauchon.dev>
2025-05-24 16:30:08 +0800

    fix db path

--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-DB_PATH=/config/SSS   java -Xmx256M -Dfile.encoding=UTF-8 -Dserver.port=9001 -jar /app/SpendSpentSpent.jar
+java -Xmx256M -Dfile.encoding=UTF-8 -Dserver.port=9001 -jar /app/SpendSpentSpent.jar
