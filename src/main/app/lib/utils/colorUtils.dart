b2eb0d09b71f699bddaab71b41a02fb6a1a545ed
office-pc nix root <paul@fauchon.dev>
2025-06-21 10:09:24 +0800

    fix some issue with the new main menu colors

--- src/main/app/lib/utils/colorUtils.dart
+++ src/main/app/lib/utils/colorUtils.dart
@@ -64,3 +64,15 @@ LinearGradient defaultGradient(BuildContext context) {
       begin: Alignment.bottomCenter,
       end: Alignment.topRight);
 }
+
+Color darken(Color color, [double amount = .1]) {
+  final hsl = HSLColor.fromColor(color);
+  final hslDark = hsl.withLightness((hsl.lightness - amount).clamp(0.0, 1.0));
+  return hslDark.toColor();
+}
+
+Color brighten(Color color, [double amount = .1]) {
+  final hsl = HSLColor.fromColor(color);
+  final hslBright = hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0));
+  return hslBright.toColor();
+}
