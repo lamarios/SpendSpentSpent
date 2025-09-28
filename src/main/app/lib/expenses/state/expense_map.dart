9e4fd9a7996fcc056b1690c52927aef0538649e9
office-pc nix root <paul@fauchon.dev>
2025-09-28 13:50:58 +0800

    make expense cluster clickable in the expense map to show which expense are under the cluster

--- src/main/app/lib/expenses/state/expense_map.dart
+++ src/main/app/lib/expenses/state/expense_map.dart
@@ -7,25 +7,68 @@ import 'package:flutter_map_heatmap/flutter_map_heatmap.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:logging/logging.dart';
+import 'package:spend_spent_spent/expenses/models/day_expense.dart';
+import 'package:spend_spent_spent/expenses/models/expense_cluster.dart';
+
+import '../models/expense.dart';
 
 part 'expense_map.freezed.dart';
 
 final _log = Logger('ExpenseMapCubit');
 
 class ExpenseMapCubit extends Cubit<ExpenseMapState> {
-  final List<WeightedLatLng> heatmapData;
+  final Map<String, DayExpense> expenses;
   final MapController mapController = MapController();
-  final CameraFit? defaultFit;
   int? zoom;
 
-  ExpenseMapCubit(super.initialState, this.heatmapData, this.defaultFit) {
+  ExpenseMapCubit(super.initialState, this.expenses) {
+    init();
+  }
+
+  void init() {
+    var expenseList = expenses.values
+        .expand((e) => e.expenses)
+        .where((exp) => exp.latitude != 0 && exp.longitude != 0)
+        .toList();
+    expenseList.sort((a, b) => a.timestamp.compareTo(b.timestamp));
+
+    if (expenseList.isEmpty) {
+      emit(state.copyWith(fit: null));
+      return;
+    }
+
+    List<LatLng> points = [];
+    List<WeightedLatLng> heatmapData = [];
+    CameraFit? fit;
+    LatLng? center;
+
+    for (var exp in expenseList) {
+      var latitude = exp.latitude!;
+      var longitude = exp.longitude!;
+      var latLng = LatLng(latitude, longitude);
+      heatmapData.add(WeightedLatLng(latLng, exp.amount));
+      points.add(latLng);
+    }
+    if (expenseList.length > 1) {
+      fit = CameraFit.bounds(
+        bounds: LatLngBounds.fromPoints(points),
+        padding: EdgeInsets.all(50),
+      );
+      center = null;
+    } else {
+      center = points[0];
+      fit = null;
+    }
+
+    emit(state.copyWith(heatmapData: heatmapData, fit: fit, center: center));
+
     mapController.mapEventStream.listen((event) {
       _log.fine('event ${event.runtimeType}, zoom: ${event.camera.zoom}');
 
       if (event.camera.zoom.floor() != zoom) {
         emit(
           state.copyWith(
-            aggregatedData: aggregateByZoom(heatmapData, event.camera.zoom),
+            aggregatedData: aggregateByZoom(expenseList, event.camera.zoom),
           ),
         );
       }
@@ -51,27 +94,29 @@ class ExpenseMapCubit extends Cubit<ExpenseMapState> {
     return 6;
   }
 
-  List<WeightedLatLng> aggregateByZoom(List<WeightedLatLng> data, double zoom) {
+  List<ExpenseCluster> aggregateByZoom(List<Expense> data, double zoom) {
     final decimals = decimalsForZoom(zoom);
     final factor = pow(10, decimals);
 
-    final Map<String, List<WeightedLatLng>> buckets = {};
+    final Map<String, List<Expense>> buckets = {};
     for (final w in data) {
-      final latKey = (w.latLng.latitude * factor).round();
-      final lngKey = (w.latLng.longitude * factor).round();
+      final latKey = (w.latitude! * factor).round();
+      final lngKey = (w.longitude! * factor).round();
       final key = '$latKey-$lngKey';
       buckets.putIfAbsent(key, () => []).add(w);
     }
 
     return buckets.values.map((list) {
-      final total = list.fold<double>(0, (s, w) => s + w.intensity);
+      final total = list.fold<double>(0, (s, w) => s + w.amount);
       final avgLat =
-          list.map((w) => w.latLng.latitude).reduce((a, b) => a + b) /
-          list.length;
+          list.map((w) => w.latitude!).reduce((a, b) => a + b) / list.length;
       final avgLng =
-          list.map((w) => w.latLng.longitude).reduce((a, b) => a + b) /
-          list.length;
-      return WeightedLatLng(LatLng(avgLat, avgLng), total);
+          list.map((w) => w.longitude!).reduce((a, b) => a + b) / list.length;
+      return ExpenseCluster(
+        location: LatLng(avgLat, avgLng),
+        total: total,
+        expenses: list,
+      );
     }).toList();
   }
 
@@ -87,8 +132,11 @@ class ExpenseMapCubit extends Cubit<ExpenseMapState> {
 @freezed
 sealed class ExpenseMapState with _$ExpenseMapState {
   const factory ExpenseMapState({
-    @Default([]) List<WeightedLatLng> aggregatedData,
+    @Default([]) List<ExpenseCluster> aggregatedData,
+    @Default([]) List<WeightedLatLng> heatmapData,
     @Default(true) bool showHeatmap,
     @Default(true) bool showAmounts,
+    CameraFit? fit,
+    LatLng? center,
   }) = _ExpenseMapState;
 }
---
+++ src/main/app/lib/expenses/views/components/map_expense_list.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:spend_spent_spent/expenses/models/expense_cluster.dart';
+import 'package:spend_spent_spent/expenses/views/components/expense.dart';
+import 'package:spend_spent_spent/expenses/views/components/stylized_amount.dart';
+import 'package:spend_spent_spent/utils/dialogs.dart';
+
+class MapExpenseList extends StatelessWidget {
+  final ExpenseCluster cluster;
+
+  const MapExpenseList({super.key, required this.cluster});
+
+  static Future<void> show(
+    BuildContext context, {
+    required ExpenseCluster cluster,
+  }) async {
+    showMotorBottomSheet(
+      context: context,
+
+      child: Wrap(children: [MapExpenseList(cluster: cluster)]),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        spacing: 16,
+        children: [
+          StylizedAmount(amount: cluster.total, size: 30),
+          ConstrainedBox(
+            constraints: BoxConstraints(maxHeight: 600),
+            child: ListView.builder(
+              shrinkWrap: true,
+              padding: EdgeInsets.zero,
+              itemCount: cluster.expenses.length,
+              itemBuilder: (context, index) => OneExpense(
+                expense: cluster.expenses[index],
+                showDate: true,
+                showIcons: false,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
